ad4fe60abe174554372e23a3765375b36ece6340
reMix83 <94192627+reMix83@users.noreply.github.com>
2026-09-24 23:54:46 +0300

    Update data.js

--- data.js
+++ data.js
@@ -181,7 +181,7 @@ const SITE = {
           {
             title: "Сбер-Университет — монтаж и инфографика",
             thumb: "assets/img/works/sber.jpg",
-            link: "https://storage.yandexcloud.net/bromedia-video/%D0%A1%D0%B1%D0%B5%D1%80%20%D1%83%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82%20-%20%D0%BC%D0%BE%D0%BD%D1%82%D0%B0%D0%B6%20%D0%B8%20%D0%B8%D0%BD%D1%84%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%BA%D0%B0%20(%D1%84%D1%80%D0%B0%D0%B3%D0%BC%D0%B5%D0%BD%D1%82).mp4",
+            link: "https://storage.yandexcloud.net/bromedia-video/sber.mp4",
             description: "Ролик для Сбер-Университета: монтаж, инфографика, саунд-дизайн. 60 секунд."
           },
           {
